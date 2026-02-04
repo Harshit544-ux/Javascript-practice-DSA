58b473e7be2715d77cb66b00a8b6ab5f89ad76e1
Harshit <srivastava0798@gmail.com>
2026-02-04 22:02:14 +0530

    write the program which check the array is sorted or not

---
+++ isSortedArray.js
@@ -0,0 +1,39 @@
+//wap to check if the array is sorted or not
+
+
+// function to check if the array is sorted
+function isSortedArray(arr)
+{
+    // arr[] = [1,2,2,3,3,4]
+
+    //loop through the array
+    for (let i=1;i<=arr.length-1;i++){
+        //check if the current element is greater than the previous element
+        if(arr[i]<=arr[i-1]){
+            continue;
+        }
+        else{
+            //if the current element is not greater than the previous element, return false
+            return false;
+        }
+    }
+    //if the array is sorted, return true
+    return true;
+}
+
+
+
+//initialize the array
+const arr=[1,2,8,3,3,4];
+
+//call the function
+const result=isSortedArray(arr);
+
+//print the result
+if(result==true){
+    console.log("array is sorted")
+}
+
+if(result==false){
+    console.log("array is not sorted")
+}
